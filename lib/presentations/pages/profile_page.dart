9b373fd19e2fc8d71c78a7463f1c4a2bd73eb7e3
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2023-04-20 05:19:33 +0700

    add widgets

--- lib/presentations/pages/profile_page.dart
+++ lib/presentations/pages/profile_page.dart
@@ -24,12 +24,35 @@ class Profilepage extends StatelessWidget {
               await database.getBuyer(authController.user!.uid);
         },
         builder: (controller) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                buildProfileCard(),
-              ],
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  buildProfileCard(),
+                  const SizedBox(height: 25),
+                  title('Pengaturan Akun'),
+                  buildMenuCard([
+                    cards('Profil', Icons.person, () {}),
+                    const Divider(
+                      height: 0,
+                      thickness: 1,
+                    ),
+                    cards('Akun', Icons.account_box, () {}),
+                  ]),
+                  const SizedBox(height: 25),
+                  title('Lainnya'),
+                  buildMenuCard([
+                    cards('Bantuan', Icons.help, () {}),
+                    const Divider(
+                      height: 0,
+                      thickness: 1,
+                    ),
+                    cards('Tentang', Icons.info, () {}),
+                  ]),
+                ],
+              ),
             ),
           );
         },
@@ -83,6 +106,65 @@ class Profilepage extends StatelessWidget {
             icon: const Icon(Icons.logout),
           ),
         ],
+      ),
+    );
+  }
+
+  buildMenuCard(List<Widget> children) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xfff8f9fa),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Column(
+        children: children,
+      ),
+    );
+  }
+
+  cards(String title, IconData icon, Function() onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: const Color(0xfff8f9fa),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: const Color(0xff212529),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              title,
+              style: GoogleFonts.plusJakartaSans(
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  title(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+      child: Text(
+        title,
+        style: GoogleFonts.plusJakartaSans(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: const Color.fromARGB(255, 99, 103, 107),
+        ),
       ),
     );
   }
