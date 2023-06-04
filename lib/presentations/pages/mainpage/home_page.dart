bf29f5dc1c1f0ff560fdeca4ca34874c4e5c6493
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2023-06-04 14:12:24 +0700

    change the homepage ui

--- lib/presentations/pages/mainpage/home_page.dart
+++ lib/presentations/pages/mainpage/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:badges/badges.dart' as badges;
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:emarket_buyer/common/formatter.dart';
 import 'package:emarket_buyer/models/model.dart';
 import 'package:emarket_buyer/presentations/controller/controller.dart';
@@ -22,56 +23,58 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: categories.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Homepage'),
-            bottom: TabBar(
-              tabs: categories.map((category) {
-                if (category == 'Makanan') {
-                  return const Tab(
-                    icon: Icon(Icons.food_bank),
-                  );
-                } else if (category == 'Minuman') {
-                  return const Tab(
-                    icon: Icon(Icons.local_drink),
-                  );
-                }
-                return Tab(text: category);
-              }).toList(),
-            ),
-            actions: [
-              IconButton(
-                icon: Obx(
-                  () => badges.Badge(
-                    badgeContent: Text(
-                      cartController.cartProducts.length.toString(),
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                    showBadge:
-                        cartController.cartProducts.isNotEmpty ? true : false,
-                    badgeAnimation: const badges.BadgeAnimation.rotation(
-                      animationDuration: Duration(seconds: 1),
-                      colorChangeAnimationDuration: Duration(seconds: 1),
-                      loopAnimation: false,
-                      curve: Curves.fastOutSlowIn,
-                      colorChangeAnimationCurve: Curves.easeInCubic,
-                    ),
-                    child: const Icon(Icons.shopping_bag_rounded),
+      length: categories.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Homepage'),
+          bottom: TabBar(
+            tabs: categories.map((category) {
+              if (category == 'Makanan') {
+                return const Tab(
+                  icon: Icon(Icons.food_bank_rounded),
+                  text: 'Makanan',
+                );
+              } else if (category == 'Minuman') {
+                return const Tab(
+                  icon: Icon(Icons.local_drink_rounded),
+                  text: 'Minuman',
+                );
+              }
+              return Tab(text: category);
+            }).toList(),
+          ),
+          actions: [
+            IconButton(
+              icon: Obx(
+                () => badges.Badge(
+                  badgeContent: Text(
+                    cartController.cartProducts.length.toString(),
+                    style: const TextStyle(color: Colors.white),
                   ),
+                  showBadge:
+                      cartController.cartProducts.isNotEmpty ? true : false,
+                  badgeAnimation: const badges.BadgeAnimation.rotation(
+                    animationDuration: Duration(seconds: 1),
+                    colorChangeAnimationDuration: Duration(seconds: 1),
+                    loopAnimation: false,
+                    curve: Curves.fastOutSlowIn,
+                    colorChangeAnimationCurve: Curves.easeInCubic,
+                  ),
+                  child: const Icon(Icons.shopping_bag_rounded),
                 ),
-                onPressed: () {
-                  Get.toNamed('/cart-page');
-                },
               ),
-            ],
-          ),
-          body: TabBarView(
-            children: categories
-                .map((category) => buildProductGrid(category))
-                .toList(),
-          ),
-        ));
+              onPressed: () {
+                Get.toNamed('/cart-page');
+              },
+            ),
+          ],
+        ),
+        body: TabBarView(
+          children:
+              categories.map((category) => buildProductGrid(category)).toList(),
+        ),
+      ),
+    );
   }
 
   buildSellerList() {
@@ -117,21 +120,42 @@ class Homepage extends StatelessWidget {
               child: GridTile(
                 key: ValueKey(filteredList[index].id),
                 footer: GridTileBar(
-                  backgroundColor: Colors.black54,
-                  title: Text(
-                    filteredList[index].name,
-                    textAlign: TextAlign.center,
-                  ),
-                  subtitle: Text(
-                    Formatter.priceFormat(filteredList[index].price),
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
+                    backgroundColor: Colors.black54,
+                    title: Text(
+                      filteredList[index].name,
+                      textAlign: TextAlign.center,
                     ),
+                    subtitle: Text(
+                      Formatter.priceFormat(filteredList[index].price),
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.shopping_cart_rounded),
+                      onPressed: () {
+                        cartController.addProduct(CartModel(
+                            productId: filteredList[index].id,
+                            name: filteredList[index].name,
+                            price: filteredList[index].price,
+                            imageUrl: filteredList[index].imageUrl,
+                            sellerId: filteredList[index].sellerId,
+                            storeName: sellerController.sellers
+                                .firstWhere(
+                                  (element) =>
+                                      element.id ==
+                                      filteredList[index].sellerId,
+                                  orElse: () => const SellerModel(),
+                                )
+                                .storeName));
+                      },
+                    )),
+                child: CachedNetworkImage(
+                  imageUrl: filteredList[index].imageUrl,
+                  placeholder: (context, url) => const Center(
+                    child: CircularProgressIndicator(),
                   ),
-                ),
-                child: Image.network(
-                  filteredList[index].imageUrl,
                   fit: BoxFit.cover,
                 ),
               ),
